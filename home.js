b2c581f0f3701483cfb4b171367f3677df110930
Paulin <shemampa2010@gmail.com>
2026-08-11 18:29:10 +0200

    every product has it own box

--- home.js
+++ home.js
@@ -641,9 +641,11 @@ let productsContainer = document.getElementById("products");
 
 clothes.forEach(function(product) {
     productsContainer.innerHTML += `
+    <div class= "product">
         <img src="${product.images[0]}" width="200">
         <h2>${product.title}</h2>
         <p>$${product.price}</p>
+    </div>
     `;
 });
 
