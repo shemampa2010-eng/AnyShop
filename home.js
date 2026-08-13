57527c1281caac1ef1ceace67cc79fdf9239aeac
Paulin <shemampa2010@gmail.com>
2026-08-13 13:27:09 +0200

    the nav is now fonctional

--- home.js
+++ home.js
@@ -668,12 +668,63 @@ boxes.forEach(function(box) {
 
 let women = document.getElementById("Femme");
 let men = document.getElementById("Homme");
+let womenClothes = clothes.filter(function(product){
+  return product.category === "women's clothing"
+})
+
+let menClothes = clothes.filter(function(product){
+  return product.category === "men's clothing"
+})
+let unisex = document.getElementById("Unisex");
+let unisexClothes = clothes.filter(function(product) {
+    return product.category === "unisex";
+});
 
 
 women.addEventListener("click", function() {
-    console.log("Femme");
+
+    productsContainer.innerHTML = "";
+
+    womenClothes.forEach(function(product) {
+        productsContainer.innerHTML += `
+            <div class="product">
+                <img src="${product.images[0]}" width="200">
+                <h2>${product.title}</h2>
+                <p>$${product.price}</p>
+            </div>
+        `;
+    });
+
 });
 
 men.addEventListener("click", function() {
-    console.log("Homme");
+
+    productsContainer.innerHTML = "";
+
+      menClothes.forEach(function(product) {
+        productsContainer.innerHTML += `
+            <div class="product">
+                <img src="${product.images[0]}" width="200">
+                <h2>${product.title}</h2>
+                <p>$${product.price}</p>
+            </div>
+        `;
+    });
+
+});
+
+unisex.addEventListener("click", function() {
+
+    productsContainer.innerHTML = "";
+
+    unisexClothes.forEach(function(product) {
+        productsContainer.innerHTML += `
+            <div class="product">
+                <img src="${product.images[0]}" width="200">
+                <h2>${product.title}</h2>
+                <p>$${product.price}</p>
+            </div>
+        `;
+    });
+
 });
