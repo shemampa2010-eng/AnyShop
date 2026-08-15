85ba1d0f61ae1585c7e0cc66bc137ab1b4c13481
Paulin <shemampa2010@gmail.com>
2026-08-15 16:44:53 +0200

    cart can show some informations about the products in there

--- cart.js
+++ cart.js
@@ -1,10 +1,18 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+let cartContainer = document.querySelector("#cart");
+
 cart.forEach(function(item) {
 
     let product = clothes.find(function(product) {
-        return product.id == item.id;
+        return product.id == item.product;
     });
 
-    console.log(product);
+    cartContainer.innerHTML += `
+        <div class="cart-product">
+            <h2>${product.title}</h2>
+            <p>$${product.price}</p>
+            <p>Size: ${item.size}</p>
+        </div>
+    `;
 });
