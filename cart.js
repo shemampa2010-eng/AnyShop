0f53431715eb641f701f1f7e39092afc210b2770
Paulin <shemampa2010@gmail.com>
2026-08-15 17:00:40 +0200

    Cart can show the image of the items

--- cart.js
+++ cart.js
@@ -2,6 +2,8 @@ let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 let cartContainer = document.querySelector("#cart");
 
+let total = 0;
+
 cart.forEach(function(item) {
 
     let product = clothes.find(function(product) {
@@ -10,9 +12,23 @@ cart.forEach(function(item) {
 
     cartContainer.innerHTML += `
         <div class="cart-product">
+            <img src="${product.images[0]}" width="150">
             <h2>${product.title}</h2>
             <p>$${product.price}</p>
             <p>Size: ${item.size}</p>
         </div>
     `;
 });
+
+cart.forEach(function(item) {
+
+    let product = clothes.find(function(product) {
+        return product.id == item.product;
+    });
+
+    total += product.price;
+});
+
+cartContainer.innerHTML += `
+    <h2>Total : $${total.toFixed(2)}</h2>
+`;
