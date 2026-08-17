a6bac99cf68012baa31e7674326bbf7225f2d198
Paulin <shemampa2010@gmail.com>
2026-08-17 11:50:57 +0200

    used event delegations

--- home.js
+++ home.js
@@ -7,43 +7,43 @@ clothes.forEach(function(product) {
             <img class="product-image" src="${product.images[0]}">
             <h2>${product.title}</h2>
             <p>$${product.price}</p>
-            <button class= "add">add to cart</button>
+            <button class="add">add to cart</button>
         </div>
     `;
 
 });
 
-let addToCart = document.querySelectorAll(".add")
-let boxes = document.querySelectorAll(".product");
 
-addToCart.forEach(function(button){
-  button.addEventListener("click", function () {
-    console.log(button.parentElement.dataset.id)
-  })
-})
+productsContainer.addEventListener("click", function(event) {
 
-boxes.forEach(function(box) {
+    if (event.target.matches(".add")) {
+        console.log("Add to cart");
+        console.log(event.target.parentElement.dataset.id);
+        return;
+    }
 
-    box.addEventListener("click", function() {
-
-        let id = box.dataset.id;
+    if (event.target.matches(".product")) {
+        let id = event.target.dataset.id;
 
         window.location.href = `product.html?id=${id}`;
-
-    });
+    }
 
 });
 
+
 let women = document.getElementById("Femme");
 let men = document.getElementById("Homme");
-let womenClothes = clothes.filter(function(product){
-  return product.category === "women's clothing"
-})
-
-let menClothes = clothes.filter(function(product){
-  return product.category === "men's clothing"
-})
 let unisex = document.getElementById("Unisex");
+
+
+let womenClothes = clothes.filter(function(product) {
+    return product.category === "women's clothing";
+});
+
+let menClothes = clothes.filter(function(product) {
+    return product.category === "men's clothing";
+});
+
 let unisexClothes = clothes.filter(function(product) {
     return product.category === "unisex";
 });
@@ -54,45 +54,56 @@ women.addEventListener("click", function() {
     productsContainer.innerHTML = "";
 
     womenClothes.forEach(function(product) {
+
         productsContainer.innerHTML += `
-            <div class="product">
+            <div class="product" data-id="${product.id}">
                 <img src="${product.images[0]}" width="200">
                 <h2>${product.title}</h2>
                 <p>$${product.price}</p>
+                <button class="add">add to cart</button>
             </div>
         `;
+
     });
 
 });
+
 
 men.addEventListener("click", function() {
 
     productsContainer.innerHTML = "";
 
-      menClothes.forEach(function(product) {
+    menClothes.forEach(function(product) {
+
         productsContainer.innerHTML += `
-            <div class="product">
+            <div class="product" data-id="${product.id}">
                 <img src="${product.images[0]}" width="200">
                 <h2>${product.title}</h2>
                 <p>$${product.price}</p>
+                <button class="add">add to cart</button>
             </div>
         `;
+
     });
 
 });
+
 
 unisex.addEventListener("click", function() {
 
     productsContainer.innerHTML = "";
 
     unisexClothes.forEach(function(product) {
+
         productsContainer.innerHTML += `
-            <div class="product">
+            <div class="product" data-id="${product.id}">
                 <img src="${product.images[0]}" width="200">
                 <h2>${product.title}</h2>
                 <p>$${product.price}</p>
+                <button class="add">add to cart</button>
             </div>
         `;
+
     });
 
 });
