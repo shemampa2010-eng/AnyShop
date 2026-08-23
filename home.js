77b869c2471c8f16ed9a9a93d44c93d12fdfb649
Paulin <shemampa2010@gmail.com>
2026-08-23 14:13:08 +0200

    can sort by name

--- home.js
+++ home.js
@@ -143,5 +143,12 @@ sort.addEventListener("change", function(){
             return b.price - a.price;
         });
     }
+    if (sort.value === "name"){
+        copy.sort(function(a, b){
+            if(a.title < b.title) return -1 ;
+            if(a.title > b.title) return 1 ;
+            return 0;
+        })
+    }
     displayProducts(copy);
 });
