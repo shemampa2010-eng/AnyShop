593adf40dd2203bd5b6cba98e2dbfbb2c5a05f39
Paulin <shemampa2010@gmail.com>
2026-09-05 14:10:31 +0200

    the checkout page shows the total

--- payment.js
+++ payment.js
@@ -2,11 +2,19 @@ let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 let container = document.getElementById("orderSummary");
 
+let subtotalElement = document.getElementById("subtotal");
+
+let finalTotalElement = document.getElementById("finalTotal");
+
+let total = 0
+
+let shippingFee = 5.00
+
 cart.forEach(function(item) {
     let match = clothes.find(function(p) {
         return p.id == item.product;
     });
-    let total = 0
+    
     if(match) {
         container.innerHTML += `<p>Produit:  ${match.title} | Quantité: ${item.quantity}</p>`;
         
@@ -16,3 +24,8 @@ cart.forEach(function(item) {
     }
     
 });
+
+subtotalElement.textContent = `$${total.toFixed(2)}`;
+
+let finalTotal = total + shippingFee
+finalTotalElement.textContent = `$${finalTotal.toFixed(2)}`;
